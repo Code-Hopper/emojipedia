337d86abf1e497cbf3a3f45b81f834d38faa66f9
code_hopper <ameykhondekar01@gmail.com>
2024-03-08 11:03:03 +0530

    updates

--- src/components/DisplatEmojise.jsx
+++ src/components/DisplatEmojise.jsx
@@ -1,25 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const DisplatEmojise = (props) => {
 
     let DisplayEachEmoji = (props) => {
 
-        let handelDelete = () =>{
+        let [isEdit , setIsEdit] = useState(false)
+
+        let handelDelete = () => {
             // alert(`delete called by ${props.index}`)
             props.onDelete(props.index)
+        }
+
+        let handelEdit = () =>{
+            setIsEdit(!isEdit)
         }
 
         return (
             <div key={props.index} className="col-3">
                 <div className='card p-4 shadow-lg text-center'>
-                    <h2>{props.emoji.icon}</h2>
-                    <h3>{props.emoji.name}</h3>
-                    <p>{props.emoji.discription}</p>
+                    <h2>
+                        <input disabled={ ! isEdit} className='form-control' type="text" value={props.emoji.icon} />
+                    </h2>
+                    <h3>
+                        <input disabled={ ! isEdit} className='form-control' type="text" value={props.emoji.name} />
+                    </h3>
+                    <p>
+                        <input disabled={ ! isEdit} className='form-control' type="text" value={props.emoji.discription} />
+                    </p>
 
                     <div className='my-2 d-flex gap-2 justify-content-center align-items-center'>
                         <button onClick={handelDelete} className='btn btn-danger'>
                             Delete
                         </button>
+
+                        <button onClick={handelEdit} className='btn btn-primary'>
+                            Edit
+                        </button>
+
                     </div>
 
                 </div>
@@ -36,7 +53,7 @@ const DisplatEmojise = (props) => {
                         {
                             props.array.map((emoji, index) => {
                                 return (
-                                    < DisplayEachEmoji emoji={emoji} index={index} onDelete={props.onDelete}/>
+                                    < DisplayEachEmoji emoji={emoji} index={index} onDelete={props.onDelete} />
                                 )
                             })
                         }
